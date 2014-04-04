fd392dd42117e0bba4c054014bc687f0ee6800c0
Saint0fCloud <saint0fcloud@gmail.com>
2014-04-03 21:45:36 -0500

    Prototype script

---
+++ zerve.zsh
@@ -0,0 +1,277 @@
+function zerve {
+    case $1 in
+        ("stop")
+            __zerve:cleanup 2>/dev/null
+            return;;
+        (*)
+            if [[ -d $1 || -f $1 ]]; then
+                _ZRV_DOCROOT="$1"
+            else
+                _ZRV_DOCROOT="$PWD"
+            fi;;
+    esac
+
+    print "Made it past with $@"
+
+    zmodload zsh/datetime
+    zmodload zsh/net/tcp
+    zmodload zsh/stat
+    zmodload zsh/system
+
+    _ZRV_PORT=8080
+    _ZRV_DOCROOT="$PWD"
+
+    setopt local_options nomultibyte
+
+    __http:listen || { print "No good"; return 1 }
+    _ZRV_LISTENFD=$REPLY
+
+    zle -F $_ZRV_LISTENFD __zerve:handler
+}
+
+function __zerve:cleanup {
+    print "Closing zerve..."
+    zle -F $_ZRV_LISTENFD
+    ztcp -c
+
+    unset _ZRV_PORT _ZRV_DOCROOT _ZRV_LISTENFD
+
+    zmodload -u zsh/datetime
+    zmodload -u zsh/net/tcp
+    zmodload -u zsh/stat
+    zmodload -u zsh/system
+
+    unset _ZRV_PORT _ZRV_DOCROOT _ZRV_LISTENFD
+}
+
+function __zerve:handler {
+    local fd
+
+    __http:accept $1
+    fd=$REPLY
+
+    while :; do
+        unset req_headers; local -A req_headers
+        if __http:parse_request && __http:check_request; then
+            __zerve:srv 1>&$fd 2>/dev/null || break
+            [[ $req_headers[connection] == "close" ]] && break
+        else
+            break
+        fi
+    done
+
+    ztcp -c $fd
+}
+
+function __zerve:srv {
+    local pathname
+
+    pathname="${_ZRV_DOCROOT}$(__url:decode $req_headers[url])"
+    { if [[ -f $pathname ]]; then
+        __http:return_header "200 Ok" "Content-type: $(__util:mtype $pathname); charset=UTF-8" "Content-Length: $(stat -L +size $pathname)"
+        __http:send_raw $pathname
+    elif [[ -d $pathname ]]; then
+        if [[ -f $pathname/index.html ]]; then
+            __http:return_header "200 Ok" "Content-type: $(__util:mtype $pathname/index.html); charset=UTF-8" "Content-Length: $(stat -L +size $pathname/index.html)"
+            __http:send_raw $pathname/index.html
+        else
+            __http:return_header "200 Ok" "Content-type: text/html; charset=UTF-8" "Transfer-Encoding: chunked"
+            { __util:dir_list $pathname | __util:html_template $pathname | __http:send_chunk  } || print "Oh no" >> log
+        fi
+    else
+        __http:error_header 404
+    fi } || __http:error_header 500
+}
+
+function __http:listen {
+    ztcp -l $_ZRV_PORT 2>/dev/null || { print "Could not bind to port $_ZRV_PORT" >&2; return 1 }
+    print "Listening on $_ZRV_PORT"
+}
+
+function __http:accept {
+    ztcp -a $1
+}
+
+function __http:parse_request {
+    local method url version line key value
+
+    read -t 15 -r -u $fd line || return 1
+    for method url version in ${(s. .)line%$'\r'}; do
+        req_headers[method]="$method"
+        req_headers[url]="${url%\?*}"
+        req_headers[version]="$version"
+    done
+
+    while read -t 15 -r -u $fd line; do
+        [[ -n $line && $line != $'\r' ]] || break
+        for key value in ${(s/: /)line%$'\r'}; do
+            req_headers[${(L)key}]="$value"
+        done
+    done
+}
+
+function __http:error_header {
+    local message
+
+    case "$1" in
+        (301)
+            message="301 Moved Permanently"
+            add_headers+="Location: $2";;
+        (404)
+            message="404 Not Found";;
+        (500)
+            message="500 Internal Server Error";;
+        (501)
+            message="501 Not Implemented";;
+        (505)
+            message="505 HTTP Version Not Supported";;
+    esac
+
+    __http:return_header "$message" "Content-type: text/plain; charset=UTF-8" "Content-Length: ${#message}" $add_headers[@]
+    print "$message"
+}
+
+function __http:return_header {
+    print -n "HTTP/1.1 $1\r\n"
+    print -n "Connection: ${req_headers[connection]:-keep-alive}\r\n"
+    print -n "Date: $(export TZ=UTC && strftime "%a, %d %b %Y %H:%M:%S" $EPOCHSECONDS) GMT\r\n"
+    print -n "Server: czhttpd\r\n"
+    for i in "$@[2,-1]"; do
+        print -n "$i\r\n"
+    done
+    print -n "\r\n"
+}
+
+function __http:check_request {
+    if [[ $req_headers[version] != "HTTP/1.1" ]]; then
+        __http:error_header 505
+        return 1
+    elif [[ $req_headers[method] != "GET" ]]; then
+        __http:error_header 501
+        return 1
+    fi
+}
+
+function __http:send_raw {
+    <$1
+}
+
+function __http:send_chunk {
+    while sysread buff; do
+        printf '%x\r\n' "$(print ${#buff})" || print "First print failed" >> log
+        printf '%s\r\n' "$buff" || print "Second print failed" >> log
+    done
+
+    printf '%x\r\n' "0" || print "Third print failed" >> log
+    printf '\r\n' || print "Fourth print failed" >> log
+}    
+
+function __url:encode {
+    for i in ${(s::)1}; do
+        case "$i" in
+            ([-._~A-Za-z0-9])
+                printf '%s' "$i";;
+            (*)
+                printf '%%%02x' "'$i";;
+        esac
+    done
+
+    printf '\n'
+}
+
+function __url:decode {
+    printf '%b\n' "${1:gs/%/\\x}"
+}
+
+function __util:dir_list {
+    cd "$1" || return 1
+
+    for i in ./.*/(Nr) ./.*(.Nr) ./*/(Nr) ./*(.NR); do
+        __util:html_fragment $i
+    done
+}
+
+function __util:calc_size {
+    [[ -d "$1" ]] && { print "\-"; return }
+
+    local KB=1024.0
+    local MB=1048576.0
+    local GB=1073741824.0
+    
+    local size=$(stat -L +size $1)
+
+    (( $size < $KB )) && { printf '%.1f%s\n' "${size}" "B" && return }
+    (( $size < $MB )) && { printf '%.1f%s\n' "$((size/$KB))" "K" && return }
+    (( $size < $GB )) && { printf '%.1f%s\n' "$((size/$MB))" "M" && return }
+    (( $size > $GB )) && { printf '%.1f%s\n' "$((size/$GB))" "G" && return }
+}
+
+function __util:html_fragment {
+    print "<tr><td><a href="${i#*/}">${i#*/}</a></td>"
+    print "<td>$(__util:calc_size $i)</td>"
+    print "<td>$(__util:mime_type $i)</td></tr>"
+}
+
+function __util:html_template {
+<<EOF
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+a {text-decoration: none;}
+a:hover, a:focus { color: white; background: rgba(0,0,0,0.3); cursor: pointer; }
+h2 { margin-bottom: 10px }
+table { border-collapse: collapse; }
+thead th { padding-top: 4px; padding-bottom: 6px; text-align: left; }
+thead th:nth-child(2) { text-align: right; padding-right: 12px; }
+tbody td:nth-child(2) { text-align: right; padding-right: 12px; }
+tbody td:first-child { padding-right: 30px; }
+div.list { background-color: #F5F5F5; border-top: 1px solid black; border-bottom: 1px solid black; font: 90% monospace; margin: 4px;}
+div.footer { font: 90% monospace; color: grey; font-style: italic; padding-left: 4px; }
+</style>
+<title>czhttpd</title>
+</head>
+<body>
+<h2>Index of $1</h2>
+<div class=list>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Size</th>
+            <th>Type</th>
+        </tr>
+        </thead>
+        <tbody>
+EOF
+while sysread -o 1; do
+    continue
+done
+<<EOF
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
+EOF
+}
+
+function __util:mime_type {
+    [[ -d "$1" ]] && { print "Dir"; return }
+
+    case $1 in
+        (*.html)
+            print "text/html";;
+        (*.css)
+            print "text/css";;
+        (*)
+            if which file >/dev/null; then
+                local mtype
+                mtype=$(file -bL --mime-type $1)
+                [[ ${mtype:h} == text ]] && { print "text/plain"; continue }
+                print "${mtype#application/x-executable}"
+            else
+                print "application/octet-stream"
+            fi;;
+    esac
+}
